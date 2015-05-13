4ea7ff75da803aba1e479435cd3799d5722409b8
Yann Gouffon <github@yago-art.ch>
2015-05-13 17:32:16 +0200

    Update 2015-05-13T15:32:13.726Z

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+(function ($) {
+  $(document).ready(function(){
+    $('.focuspoint').focusPoint();
+  });
+}(jQuery));
+'use strict';
+
 (function(){
 
 }());
+
+'use strict';
+
+(function($){
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+}(jQuery));
