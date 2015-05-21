f626e497f8cc32143f0a5d518fa15504ed200723
Yann Gouffon <github@yago-art.ch>
2015-05-21 15:51:24 +0200

    apply hover icon-button effect to parent also

--- assets/js/icon-button.js
+++ assets/js/icon-button.js
@@ -2,19 +2,26 @@
 
 (function($){
   $(document).ready(function(){
-    $('.icon-button').mouseenter(function(){
+    $('.icon-button, .thumbnail-cover a, .thumbnail').mouseenter(function(){
       var $that = $(this),
-          widthAuto = $that.find('.button-label').width(),
-          halfDisc = $that.width() / 2;
+          widthAuto = $that.find('.button-label').width();
 
-      $that.find('.button-label-wrapper').animate({width: widthAuto + 50}, 150, function(){
-        $that.find('.button-label').animate({opacity: 1}, 150);
-      });
+      $that.find('.button-label-wrapper').animate({width: widthAuto + 50}, 150);
+      $that.find('.button-label').animate({opacity: 1}, 300);
     });
 
     $('.icon-button').mouseleave(function(){
       var $that = $(this),
           halfDisc = $(this).width() / 2;
+
+      $that.find('.button-label').animate({opacity: 0}, 150, function(){
+        $that.find('.button-label-wrapper').animate({width: halfDisc}, 150);
+      });
+    });
+
+    $('.thumbnail-cover a, .thumbnail').mouseleave(function(){
+      var $that = $(this),
+          halfDisc = $(this).find('.icon-button').width() / 2;
 
       $that.find('.button-label').animate({opacity: 0}, 150, function(){
         $that.find('.button-label-wrapper').animate({width: halfDisc}, 150);
